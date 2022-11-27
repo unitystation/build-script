362134adae324c4b31e1ebaca7a16f161605665e
Eugene <fogaprod@gmail.com>
2022-11-27 15:48:22 +0400

    remove python 2 nonsense

--- usautobuild/exceptions.py
+++ usautobuild/exceptions.py
@@ -1,48 +1,55 @@
 class BaseException(Exception):
-    pass
+    ...
 
 
 class MissingRequiredEnv(BaseException):
     def __init__(self, name_env):
-        super(MissingRequiredEnv, self).__init__(
+        super().__init__(
             f"Missing a required environmental variable: {name_env}"
         )
 
+
 class MissingRequiredConfig(BaseException):
     def __init__(self, name_config):
-        super(MissingRequiredConfig, self).__init__(
+        super().__init__(
             f"Missing a required configuration from the config file: {name_config}"
         )
 
+
 class MissingConfigFile(BaseException):
     def __init__(self, path):
-        super(MissingConfigFile, self).__init__(
+        super().__init__(
             f"Couldn't find config file in the set directory: {path}"
         )
 
+
 class InvalidConfigFile(BaseException):
     def __init__(self):
-        super(InvalidConfigFile, self).__init__(
+        super().__init__(
             "The config file seems to be an invalid JSON file."
         )
 
+
 class NoChanges(BaseException):
     def __init__(self, branch: str):
-        super(NoChanges, self).__init__(
+        super().__init__(
             f"Found no changes on branch {branch}. Aborting the build!"
         )
 
+
 class InvalidProjectPath(BaseException):
     def __init__(self):
-        super(InvalidProjectPath, self).__init__(
+        super().__init__(
             "Path to unity project couldn't was invalid!"
         )
 
+
 class BuildFailed(BaseException):
     def __init__(self, target):
-        super(BuildFailed, self).__init__(
+        super().__init__(
             f"Build for {target} failed!"
         )
+
 
 class MissingLicenseFile(BaseException):
     def __init__(self, path):
