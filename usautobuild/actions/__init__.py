d0e291c787233993e542ae1b9d1961dbd8e0d091
Eugene <fogaprod@gmail.com>
2022-12-03 19:36:55 +0400

    move actions to a separate folder

---
+++ usautobuild/actions/__init__.py
@@ -0,0 +1,15 @@
+from .api_caller import ApiCaller
+from .builder import Builder
+from .dockerizer import Dockerizer
+from .gitter import Gitter
+from .licenser import Licenser
+from .uploader import Uploader
+
+__all__ = (
+    "ApiCaller",
+    "Builder",
+    "Dockerizer",
+    "Gitter",
+    "Licenser",
+    "Uploader",
+)
