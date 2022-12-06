55745cced5f50a6382b9c982731ce6c5ef88f8df
Eugene <fogaprod@gmail.com>
2022-12-07 00:21:01 +0400

    add switch that only prints stderr when error happened (#18)

--- usautobuild/actions/dockerizer.py
+++ usautobuild/actions/dockerizer.py
@@ -46,6 +46,8 @@ class Dockerizer:
 
         if status := run_process_shell(
             'echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin',
+            # complains about storing credentials in filesystem
+            stderr_on_failure=True,
         ):
             raise Exception(f"Docker login failed: {status}")
 
